5afc64bc107022fd1448f5ef322d70fcb21b8b52
Ryxian <Ryxians@gmail.com>
2021-08-30 18:32:30 -0500

    Make navbar function properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Navbar} from "./components/navbar/Navbar";
-import {Start} from "./components/form/start/Start";
-import {BootStrapLogin} from "./components/login/BootStrapLogin";
+import {NewKit} from "./components/form/winecreation/NewKit";
+import {BootStrapLogin} from "./components/form/login/BootStrapLogin";
 import {Route, BrowserRouter as Router, Link, Redirect} from 'react-router-dom';
+import {Start} from "./components/form/winecreation/Start";
 
 function App() {
     const [isLoggedIn, setLoggedIn] = useState(false);
     const [hashedUser, setHashedUser] = useState("");
+    const [route, changeRoute] = useState("/");
+
     return (
         <Router>
             <div className="App text-center">
@@ -16,10 +19,15 @@ function App() {
                     (<Redirect to={{pathname: '/login'}} />)
                     :
                     (
-                        <Navbar />
+                        <>
+                            <Navbar  changeRoute={changeRoute}/>
+                            <Redirect to={{pathname: route}} />
+                        </>
+
                     )
                 }
                 <Route path="/start" exact component={Start} />
+                <Route path="/newkit" exact component={NewKit} />
                 <Route path="/login" exact render={props =>
                     (<BootStrapLogin isLoggedIn={isLoggedIn} handleLoggin={setLoggedIn} handleHashedUser={setHashedUser}/>)
                 }/>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,24 @@
 import React, {FC} from 'react';
 import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/collapse';
+import {Redirect} from 'react-router-dom';
+import {NewKit} from "../form/winecreation/NewKit";
 
 interface Props {
+    changeRoute: React.Dispatch<React.SetStateAction<string>>,
 
 }
 
-export const Navbar : FC<Props> = () => {
+export const Navbar : FC<Props> = ({changeRoute}) => {
+    const newRouteFromHref = (event:React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        event.preventDefault();
+        changeRoute(event.currentTarget.pathname);
+    }
+
  return (
      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
          <div className="container-fluid">
-             <a className="navbar-brand" href="/">Wine Maker's Assistant</a>
+             <a className="navbar-brand" href="/" onClick={newRouteFromHref}>Wine Maker's Assistant</a>
              <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                      data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                      aria-expanded="false" aria-label="Toggle navigation">
@@ -19,23 +27,43 @@ export const Navbar : FC<Props> = () => {
              <div className="collapse navbar-collapse" id="navbarSupportedContent">
                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                      <li className="nav-item">
-                         <a className="nav-link active" aria-current="page" href="/">General</a>
+                         <a className="nav-link active" aria-current="page" href="/"
+                            onClick={newRouteFromHref}
+                         >
+                             General
+                         </a>
                      </li>
                      <li className="nav-item dropdown">
-                         <a className="nav-link active dropdown-toggle" href="/" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                         <a className="nav-link active dropdown-toggle" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false"
+                         >
                              New Wine
                          </a>
                          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                             <li><a className="dropdown-item" href="/">From Existing</a></li>
-                             <li><a className="dropdown-item" href="/">New Wine</a></li>
+                             <li><a className="dropdown-item" href="/start"
+                                    onClick={newRouteFromHref}
+                             >
+                                 From Existing
+                             </a></li>
+                             <li><a className="dropdown-item"
+                                    href="/newkit"
+                                    onClick={newRouteFromHref}
+                             >New Wine</a></li>
                          </ul>
                      </li>
                      <li className="nav-item">
-                         <a className="nav-link active" aria-current="page" href="/">Continue Wine</a>
+                         <a className="nav-link active" aria-current="page" href="/"
+                            onClick={newRouteFromHref}
+                         >
+                             Continue Wine
+                         </a>
                      </li>
                      <li className="nav-item">
-                         <a className="nav-link active" aria-current="page" href="/">Wine Logs</a>
+                         <a className="nav-link active" aria-current="page" href="/"
+                            onClick={newRouteFromHref}
+                         >
+                             Wine Logs
+                         </a>
                      </li>
                  </ul>
              </div>
