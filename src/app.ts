77c88a0dee8b50516b61124800c9149939179634
Ryxian <Ryxians@gmail.com>
2021-09-01 13:24:09 -0500

    Remove comment clutter.

--- src/app.ts
+++ src/app.ts
@@ -9,16 +9,11 @@ const PORT = process.env.PORT || 5000;
 // Specify json use
 app.use(express.json());
 
-// Get list of users TOBE DELETED
-// app.get('/users', ((req: Request, res: Response) => {
-//     res.json(users);
-// }));
 
 // Create a new user, passes the express app
 CreateUser({app});
 
 // Authenticates provided username and password
-// TOBE MOVED
 LoginUser({app});
 
 
