525691a45d9583496c85b761cd27ef0d6679236c
Ryxian <Ryxians@gmail.com>
2021-09-23 17:37:20 -0500

    Implement logout, remove old code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,65 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Navbar} from "./components/navbar/Navbar";
 import {NewKit} from "./components/form/winecreation/NewKit";
 import {BootStrapLogin} from "./components/form/login/BootStrapLogin";
-import {Route, BrowserRouter as Router, Link, Redirect} from 'react-router-dom';
+import {Route, BrowserRouter as Router, Link, Redirect, useLocation} from 'react-router-dom';
 import {Start} from "./components/form/winecreation/Start";
+import {GeneralForm} from "./components/form/GeneralForm";
 
 function App() {
     const [isLoggedIn, setLoggedIn] = useState(false);
     const [hashedUser, setHashedUser] = useState("");
     const [route, changeRoute] = useState("/");
 
+    const savedHash = localStorage.getItem("hashedUser");
+    const saveProgress = true;
+
+    useEffect(() => {
+        if (savedHash && saveProgress) {
+            // console.log("Saved Hash: ", savedHash);
+            setHashedUser(savedHash);
+            setLoggedIn(true);
+            // changeRoute(window.location.pathname);
+        }
+
+
+    })
+
+    const logout = () => {
+        setLoggedIn(false);
+        setHashedUser("");
+        localStorage.clear();
+    }
+
+    const handleHashedUser = (hashedUsr: string) => {
+        setHashedUser(hashedUsr);
+        localStorage.setItem("hashedUser", hashedUsr);
+        setLoggedIn(true);
+    }
+
     return (
         <Router>
             <div className="App text-center">
                 {!isLoggedIn ?
 
-                    (<Redirect to={{pathname: '/login'}} />)
+                    (<Redirect to={{pathname: '/login'}}/>)
                     :
                     (
                         <>
-                            <Navbar  changeRoute={changeRoute}/>
-                            <Redirect to={{pathname: route}} />
+                            <Navbar changeRoute={changeRoute} logout={logout}/>
+                            <Redirect to={{pathname: route}}/>
                         </>
 
                     )
                 }
-                <Route path="/start" exact component={Start} />
-                <Route path="/newkit" exact component={NewKit} />
+                <Route path="/start" exact component={Start}/>
+                <Route path="/newkit" exact component={NewKit}/>
                 <Route path="/login" exact render={props =>
-                    (<BootStrapLogin isLoggedIn={isLoggedIn} handleLoggin={setLoggedIn} handleHashedUser={setHashedUser}/>)
+                    (<BootStrapLogin isLoggedIn={isLoggedIn}
+                                     handleHashedUser={handleHashedUser}/>)
                 }/>
+                <Route path="/" exact component={GeneralForm}/>
             </div>
         </Router>
     );
