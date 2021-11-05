3dfca0eb6cf8669dcb44c26732fe0c26872dd485
Ryxian <Ryxians@gmail.com>
2021-11-05 14:18:33 -0500

    Remove the Navbar from view when printing.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -10,7 +10,7 @@ interface Props {
 export const Navbar : FC<Props> = ({logout}) => {
 
  return (
-     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+     <nav className="navbar navbar-expand-lg navbar-dark bg-primary d-print-none">
          <div className="container-fluid">
              <Link className="navbar-brand" to="/" >Wine Maker's Assistant</Link>
              <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
