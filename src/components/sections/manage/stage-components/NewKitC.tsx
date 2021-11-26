86165db8df9479960aa974efe573a9f3455b8171
Ryxian <Ryxians@gmail.com>
2021-11-26 14:37:00 -0600

    Add informative help to kit creation, and add Kit Volume.

--- src/components/sections/manage/stage-components/NewKitC.tsx
+++ src/components/sections/manage/stage-components/NewKitC.tsx
@@ -2,6 +2,7 @@ import React, {FC} from 'react';
 import {SubmitHandler, useForm} from "react-hook-form";
 import Axios from "axios";
 import {ModalFB} from "../../ModalFB";
+import {PopoverInfo} from "../../../PopoverInfo";
 
 interface Props {
     setWine?: Function
@@ -10,6 +11,7 @@ interface Props {
 interface Inputs {
     style: string
     name: string
+    volume: number
 }
 
 export const NewKitC: FC<Props> = ({setWine}) => {
@@ -24,14 +26,30 @@ export const NewKitC: FC<Props> = ({setWine}) => {
         });
     }
 
+    const newStyleBody =
+        <>
+            <p>
+            Enter the official Kit Style.
+            </p>
+            <p>
+            The style can be found on the side of the box
+            and on the provided sticker.
+            </p>
+            <span className={"text-warning"}>Blended wines will always have a style of BLENDED.</span>
+        </>
+
     return (
         <ModalFB handleSubmit={handleSubmit} onSubmit={onSubmit} title={"New Kit"} id={"CreateNewKitModal"} modalception={true}>
             <>
 
                 <div className="input-group m-1 form-check form-check-inline">
                     <span className="input-group-text">Kit Style: </span>
-                    <input type="text" className="form-control"
-                           {...register("style", {required: true})}/>
+                    <PopoverInfo id={"NewStyleInformation"} header={"Kit Style"}
+                                 body={newStyleBody}>
+
+                        <input type="text" className="form-control"
+                               {...register("style", {required: true})}/>
+                    </PopoverInfo>
                     <input type="checkbox" className="form-check-input p-1 m-1" id="WineStyleCheck" onChange={() => {
                         setValue("style", "BLENDED");
                     }}/>
@@ -41,7 +59,25 @@ export const NewKitC: FC<Props> = ({setWine}) => {
                 </div>
                 <div className="input-group m-1">
                     <span className="input-group-text">Fancy Name: </span>
-                    <input type="text" className="form-control" {...register("name", {required: true})}/>
+                    <PopoverInfo id={"FancyNameInformation"} header={"Fancy Name"}
+                                 body={"This is the name used on the custom label."} >
+
+                        <input type="text" className="form-control" {...register("name", {required: true})}/>
+                    </PopoverInfo>
+                </div>
+                <div className="input-group m-1">
+                    <span className="input-group-text">Kit Volume: </span>
+                    <PopoverInfo id={"NewKitVolumeInfo"} header={"Kit Volume"}
+                                 body={"Kit Volume can be found on the box the kit came in."}>
+
+                        <input type="number"
+                               className="form-control"
+                               {...register("volume")}
+                        />
+                    </PopoverInfo>
+                    <span className="input-group-text">
+                        Liters
+                    </span>
                 </div>
             </>
         </ModalFB>
