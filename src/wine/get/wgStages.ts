db22ff0672fd3d47a5737d7fd2a5c222a4af4281
Ryxian <Ryxians@gmail.com>
2021-11-29 21:24:58 -0600

    If the stage has a user id, then add the user associated with it.

--- src/wine/get/wgStages.ts
+++ src/wine/get/wgStages.ts
@@ -1,12 +1,18 @@
 import {Application} from "express";
 import {Connection, EntityTarget} from "typeorm";
+import {User} from "../../database/entities/User";
 
 interface Args {
     app:Application
     connection:Connection
 }
 
-export function CreateGetPost<Entity>(obj:EntityTarget<Entity>, path:string, {app, connection}:Args) {
+interface ns {
+    user_id?:number
+    bottleTeam?:User
+}
+
+export function CreateGetPost<Entity extends ns>(obj:EntityTarget<Entity>, path:string, {app, connection}:Args) {
     // Get all the specified entities of a batch
     app.get(`/wine/get/${path}/:batchid`, async (req, res) => {
         // Declare the new wine object and get the details from the request
@@ -14,6 +20,11 @@ export function CreateGetPost<Entity>(obj:EntityTarget<Entity>, path:string, {ap
 
         const nsList = await connection.manager.find(obj, {where: {batch_id: params.batchid}});
 
+        if (nsList[0]?.user_id) {
+            for (const s of nsList) {
+                s.bottleTeam = await connection.manager.findOne(User, {where: {id: s.user_id}});
+            }
+        }
 
         res.status(nsList ? 200 : 400).send(JSON.stringify(nsList));
     });
